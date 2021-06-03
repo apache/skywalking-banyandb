6aa2453c0d3901cb3939315e63d2c7c29870cffc
Megrez Lu <lujiajing1126@gmail.com>
2021-06-03 23:42:04 +0800

    add fields to op

--- pkg/physical/op.go
+++ pkg/physical/op.go
@@ -1,18 +1,30 @@
 package physical
 
+import apiv1 "github.com/apache/skywalking-banyandb/api/fbs/v1"
+
 type tableScan struct {
+	startTime uint64
+	endTime   uint64
 }
 
 type chunkIDFetch struct {
+	chunkID uint64
 }
 
-type traceFetch struct {
+type traceIDFetch struct {
+	traceID string
 }
 
 type indexScan struct {
+	indexName string
+	startTime uint64
+	endTime   uint64
+	query     []*apiv1.PairQuery
 }
 
 type sortedMerge struct {
+	sort      apiv1.Sort
+	fieldName string
 }
 
 type pagination struct {
