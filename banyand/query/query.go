e86ee7f07fda7b847ebbbf94226bca97e1dedd59
Megrez Lu <lujiajing1126@gmail.com>
2021-06-07 12:11:41 +0800

    make build success

--- banyand/query/query.go
+++ banyand/query/query.go
@@ -29,6 +29,6 @@ type Executor interface {
 	run.PreRunner
 }
 
-func NewExecutor(ctx context.Context, idx index.Repo, series series.UniModel, schemaRepo series.SchemaRepo) (Executor, error) {
+func NewExecutor(ctx context.Context, idx index.Repo, series series.UniModel) (Executor, error) {
 	return nil, nil
 }
