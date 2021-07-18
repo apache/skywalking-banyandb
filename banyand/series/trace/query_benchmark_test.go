11f962645fcd4d4a0f5bf489e43e660cf19659ff
Megrez Lu <lujiajing1126@gmail.com>
2021-07-18 14:14:20 +0800

    add benchmark for trace fetch

--- banyand/series/trace/query_benchmark_test.go
+++ banyand/series/trace/query_benchmark_test.go
@@ -16,6 +16,7 @@ import (
 // cpu: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz
 // Benchmark_Query_ScanEntity-8   	 1982455	     35238 ns/op	   14166 B/op	     333 allocs/op
 // PASS
+// Current commit:
 // Benchmark_Query_ScanEntity-8   	 2146119	     32553 ns/op	   12923 B/op	     288 allocs/op
 func Benchmark_Query_ScanEntity(b *testing.B) {
 	baseTS := uint64((time.Now().UnixNano() / 1e6) * 1e6)
@@ -29,5 +30,28 @@ func Benchmark_Query_ScanEntity(b *testing.B) {
 			Projection: []string{"data_binary", "trace_id"},
 			State:      series.TraceStateDefault,
 		})
+	}
+}
+
+// Commit: 01c28bc0b8cccc927780a2e1cad7293cf71f9276
+// go test -bench=Benchmark_Query_FetchTrace -run=^a -benchtime=60s -benchmem ./banyand/series/trace/...
+// goos: darwin
+// goarch: amd64
+// pkg: github.com/apache/skywalking-banyandb/banyand/series/trace
+// cpu: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz
+// Benchmark_Query_FetchTrace-8   	 4519570	     16007 ns/op	    6279 B/op	     149 allocs/op
+// PASS
+// ok  	github.com/apache/skywalking-banyandb/banyand/series/trace	153.538s
+func Benchmark_Query_FetchTrace(b *testing.B) {
+	baseTS := uint64((time.Now().UnixNano() / 1e6) * 1e6)
+	ts, stopFunc := setup(b)
+	defer stopFunc()
+	setUpTestData(b, ts, testData(baseTS))
+
+	traceID := "trace_id-xxfff.111323"
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = ts.FetchTrace(traceID, series.ScanOptions{Projection: []string{"data_binary", "trace_id"}})
 	}
 }
