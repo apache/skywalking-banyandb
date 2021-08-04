03ee3a1c81b8440e7fbd39cad6e22e41a9ac16d6
Megrez Lu <lujiajing1126@gmail.com>
2021-08-04 21:37:36 +0800

    add textual index query and numerical index query
    Signed-off-by: Megrez Lu <lujiajing1126@gmail.com>

--- banyand/series/trace/service.go
+++ banyand/series/trace/service.go
@@ -97,6 +97,7 @@ func (s *service) Serve() error {
 				Name:  sMeta.name,
 				Group: sMeta.group,
 			},
+			Catalog: v1.Series_CATALOG_TRACE,
 		}
 		rules, errGetRules := s.IndexRules(context.Background(), seriesObj, nil)
 		if errGetRules != nil {
