c620f4a82d213678b39113034a979d30b3df4a9a
Megrez Lu <lujiajing1126@gmail.com>
2021-09-25 16:21:59 +0800

    fix test in query module

--- banyand/query/v2/processor_test.go
+++ banyand/query/v2/processor_test.go
@@ -22,7 +22,6 @@ import (
 	"embed"
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"os"
 	"strconv"
 	"testing"
@@ -33,7 +32,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	"github.com/apache/skywalking-banyandb/api/common"
 	"github.com/apache/skywalking-banyandb/api/data"
 	commonv2 "github.com/apache/skywalking-banyandb/api/proto/banyandb/common/v2"
 	modelv2 "github.com/apache/skywalking-banyandb/api/proto/banyandb/model/v2"
@@ -42,10 +40,8 @@ import (
 	"github.com/apache/skywalking-banyandb/banyand/metadata"
 	"github.com/apache/skywalking-banyandb/banyand/queue"
 	"github.com/apache/skywalking-banyandb/banyand/stream"
-	"github.com/apache/skywalking-banyandb/banyand/tsdb"
 	"github.com/apache/skywalking-banyandb/pkg/bus"
 	"github.com/apache/skywalking-banyandb/pkg/logger"
-	"github.com/apache/skywalking-banyandb/pkg/partition"
 	pb "github.com/apache/skywalking-banyandb/pkg/pb/v2"
 	"github.com/apache/skywalking-banyandb/pkg/query/v2/logical"
 	"github.com/apache/skywalking-banyandb/pkg/test"
@@ -78,7 +74,7 @@ func setupServices(tester *assert.Assertions) (stream.Service, queue.Queue, func
 	// Bootstrap logger system
 	tester.NoError(logger.Init(logger.Logging{
 		Env:   "dev",
-		Level: "trace",
+		Level: "info",
 	}))
 
 	// Init `Discovery` module
@@ -125,8 +121,8 @@ func setupServices(tester *assert.Assertions) (stream.Service, queue.Queue, func
 //go:embed testdata/*.json
 var dataFS embed.FS
 
-func setupQueryData(testingT *testing.T, dataFile string, streamT stream.StreamT) (baseTime time.Time) {
-	t := assert.New(testingT)
+func setupQueryData(testing *testing.T, dataFile string, stream stream.Stream) (baseTime time.Time) {
+	t := assert.New(testing)
 	var templates []interface{}
 	baseTime = time.Now()
 	content, err := dataFS.ReadFile("testdata/" + dataFile)
@@ -154,127 +150,16 @@ func setupQueryData(testingT *testing.T, dataFile string, streamT stream.StreamT
 			},
 		}
 		e.TagFamilies = append(e.TagFamilies, searchTagFamily)
-		entity, errInner := streamT.BuildEntity(e)
-		t.NoError(errInner)
-		shardID, errInner := partition.ShardID(entity.Marshal(), streamT.GetShardNum())
-		t.NoError(errInner)
-		_, errInner = streamT.Write(common.ShardID(shardID), e)
+		errInner := stream.Write(e)
 		t.NoError(errInner)
 	}
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancelFunc()
-	err = ready(ctx, t, streamT, queryOpts{
-		entity:    tsdb.Entity{tsdb.AnyEntry, tsdb.AnyEntry, tsdb.AnyEntry},
-		timeRange: tsdb.NewTimeRangeDuration(baseTime, 1*time.Hour),
-	})
-	require.NoError(testingT, err)
 	return baseTime
-}
-
-func ready(ctx context.Context, t *assert.Assertions, stream stream.StreamT, options queryOpts) error {
-	for {
-	loop:
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			data, err := queryData(t, stream, options)
-			if err != nil {
-				return err
-			}
-			for _, d := range data {
-				if len(d.elements) < 1 {
-					time.Sleep(300 * time.Millisecond)
-					break loop
-				}
-			}
-			return nil
-		}
-	}
-}
-
-type queryOpts struct {
-	entity    tsdb.Entity
-	timeRange tsdb.TimeRange
-	buildFn   func(builder tsdb.SeekerBuilder)
-}
-
-type shardStruct struct {
-	id       common.ShardID
-	location []string
-	elements []string
-}
-
-type shardsForTest []shardStruct
-
-func queryData(tester *assert.Assertions, s stream.StreamT, opts queryOpts) (shardsForTest, error) {
-	shards, err := s.Shards(opts.entity)
-	tester.NoError(err)
-	got := shardsForTest{}
-	for _, shard := range shards {
-		seriesList, err := shard.Series().List(tsdb.NewPath(opts.entity))
-		if err != nil {
-			return nil, err
-		}
-		for _, series := range seriesList {
-			got, err = func(g shardsForTest) (shardsForTest, error) {
-				sp, errInner := series.Span(opts.timeRange)
-				defer func(sp tsdb.SeriesSpan) {
-					_ = sp.Close()
-				}(sp)
-				if errInner != nil {
-					return nil, errInner
-				}
-				builder := sp.SeekerBuilder()
-				if opts.buildFn != nil {
-					opts.buildFn(builder)
-				}
-				seeker, errInner := builder.Build()
-				if errInner != nil {
-					return nil, errInner
-				}
-				iter, errInner := seeker.Seek()
-				if errInner != nil {
-					return nil, errInner
-				}
-				for dataFlowID, iterator := range iter {
-					var elements []string
-					for iterator.Next() {
-						tagFamily, errInner := s.ParseTagFamily("searchable", iterator.Val())
-						if errInner != nil {
-							return nil, errInner
-						}
-						for _, tag := range tagFamily.GetTags() {
-							if tag.GetKey() == "trace_id" {
-								elements = append(elements, tag.GetValue().GetStr().GetValue())
-							}
-						}
-					}
-					_ = iterator.Close()
-					g = append(g, shardStruct{
-						id: shard.ID(),
-						location: []string{
-							fmt.Sprintf("series_%v", series.ID()),
-							"data_flow_" + strconv.Itoa(dataFlowID),
-						},
-						elements: elements,
-					})
-				}
-
-				return g, nil
-			}(got)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return got, nil
 }
 
 func TestQueryProcessor(t *testing.T) {
 	assertT := assert.New(t)
 	streamSvc, pipeline, deferFunc := setupServices(assertT)
-	stm, err := streamSvc.StreamT(&commonv2.Metadata{Name: "sw", Group: "default"})
+	stm, err := streamSvc.Stream(&commonv2.Metadata{Name: "sw", Group: "default"})
 	defer func() {
 		_ = stm.Close()
 		deferFunc()
