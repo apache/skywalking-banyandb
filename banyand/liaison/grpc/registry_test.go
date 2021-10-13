c1a0eaedbafcc7f7519c6369a0fbaa557c5834e1
Megrez Lu <lujiajing1126@gmail.com>
2021-10-13 22:33:27 +0800

    fix test
    Signed-off-by: Megrez Lu <lujiajing1126@gmail.com>

--- banyand/liaison/grpc/registry_test.go
+++ banyand/liaison/grpc/registry_test.go
@@ -4,18 +4,16 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 
 	commonv1 "github.com/apache/skywalking-banyandb/api/proto/banyandb/common/v1"
 	databasev1 "github.com/apache/skywalking-banyandb/api/proto/banyandb/database/v1"
 	"github.com/apache/skywalking-banyandb/banyand/metadata/schema"
 )
 
-func TestRegistry(t *testing.T) {
+func TestStreamRegistry(t *testing.T) {
 	req := require.New(t)
 	gracefulStop := setup(req, testData{
 		TLS:  false,
@@ -30,98 +28,39 @@ func TestRegistry(t *testing.T) {
 	client := databasev1.NewStreamRegistryClient(conn)
 	req.NotNil(client)
 
-	testCases := []struct {
-		name               string
-		meta               *commonv1.Metadata
-		respExtractor      func(*databasev1.EntityGetResponse) proto.Message
-		createReqGenerator func(*databasev1.EntityGetResponse) *databasev1.EntityCreateOrUpdateRequest
-	}{
-		{
-			name: "Stream Registry",
-			meta: &commonv1.Metadata{Name: "sw", Group: "default"},
-			respExtractor: func(response *databasev1.EntityGetResponse) proto.Message {
-				return response.GetStream()
-			},
-			createReqGenerator: func(resp *databasev1.EntityGetResponse) *databasev1.EntityCreateOrUpdateRequest {
-				return &databasev1.EntityCreateOrUpdateRequest{
-					Entity: &databasev1.EntityCreateOrUpdateRequest_Stream{
-						Stream: resp.GetStream(),
-					},
-				}
-			},
-		},
-		{
-			name:       "IndexRuleBinding Registry",
-			meta:       &commonv1.Metadata{Name: "sw-index-rule-binding", Group: "default"},
-			entityType: databasev1.EntityType_ENTITY_TYPE_INDEX_RULE_BINDING,
-			respExtractor: func(response *databasev1.EntityGetResponse) proto.Message {
-				return response.GetIndexRuleBinding()
-			},
-			createReqGenerator: func(resp *databasev1.EntityGetResponse) *databasev1.EntityCreateOrUpdateRequest {
-				return &databasev1.EntityCreateOrUpdateRequest{
-					Entity: &databasev1.EntityCreateOrUpdateRequest_IndexRuleBinding{
-						IndexRuleBinding: resp.GetIndexRuleBinding(),
-					},
-				}
-			},
-		},
-		{
-			name:       "IndexRule Registry",
-			meta:       &commonv1.Metadata{Name: "db.instance", Group: "default"},
-			entityType: databasev1.EntityType_ENTITY_TYPE_INDEX_RULE,
-			respExtractor: func(response *databasev1.EntityGetResponse) proto.Message {
-				return response.GetIndexRule()
-			},
-			createReqGenerator: func(resp *databasev1.EntityGetResponse) *databasev1.EntityCreateOrUpdateRequest {
-				return &databasev1.EntityCreateOrUpdateRequest{
-					Entity: &databasev1.EntityCreateOrUpdateRequest_IndexRule{
-						IndexRule: resp.GetIndexRule(),
-					},
-				}
-			},
-		},
+	meta := &commonv1.Metadata{
+		Group: "default",
+		Name:  "sw",
 	}
 
-	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {
-			tester := assert.New(t)
-			// 1 - GET -> Not Nil
-			getResp, err := client.Get(context.TODO(), &databasev1.GeneralEntityRequest{
-				Metadata:   tt.meta,
-				EntityType: tt.entityType,
-			})
-			tester.NoError(err)
-			tester.NotNil(getResp)
-			tester.NotNil(tt.respExtractor(getResp))
+	getResp, err := client.Get(context.TODO(), &databasev1.StreamGetRequest{Metadata: meta})
 
-			// 2 - DELETE
-			deleteResp, err := client.Delete(context.TODO(), &databasev1.GeneralEntityRequest{
-				Metadata:   tt.meta,
-				EntityType: tt.entityType,
-			})
-			tester.NoError(err)
-			tester.NotNil(deleteResp)
+	req.NoError(err)
+	req.NotNil(getResp)
 
-			// 3 - GET -> Nil
-			_, err = client.Get(context.TODO(), &databasev1.GeneralEntityRequest{
-				Metadata:   tt.meta,
-				EntityType: tt.entityType,
-			})
-			errStatus, _ := status.FromError(err)
-			tester.Equal(errStatus.Message(), schema.ErrEntityNotFound.Error())
+	// 2 - DELETE
+	deleteResp, err := client.Delete(context.TODO(), &databasev1.StreamDeleteRequest{
+		Metadata: meta,
+	})
+	req.NoError(err)
+	req.NotNil(deleteResp)
+	req.True(deleteResp.GetDeleted())
 
-			// 4 - CREATE
-			_, err = client.CreateOrUpdate(context.TODO(), tt.createReqGenerator(getResp))
-			tester.NoError(err)
+	// 3 - GET -> Nil
+	_, err = client.Get(context.TODO(), &databasev1.StreamGetRequest{
+		Metadata: meta,
+	})
+	errStatus, _ := status.FromError(err)
+	req.Equal(errStatus.Message(), schema.ErrEntityNotFound.Error())
 
-			// 5 - GET - > Not Nil
-			getResp, err = client.Get(context.TODO(), &databasev1.GeneralEntityRequest{
-				Metadata:   tt.meta,
-				EntityType: tt.entityType,
-			})
-			tester.NoError(err)
-			tester.NotNil(getResp)
-			tester.NotNil(tt.respExtractor(getResp))
-		})
-	}
+	// 4 - CREATE
+	_, err = client.Create(context.TODO(), &databasev1.StreamCreateRequest{Stream: getResp.GetStream()})
+	req.NoError(err)
+
+	// 5 - GET - > Not Nil
+	getResp, err = client.Get(context.TODO(), &databasev1.StreamGetRequest{
+		Metadata: meta,
+	})
+	req.NoError(err)
+	req.NotNil(getResp)
 }
