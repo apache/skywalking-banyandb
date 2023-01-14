5b222c48c468faddb7549a9b58827c3b49b76198
Megrez Lu <lujiajing1126@gmail.com>
2023-01-14 22:10:18 +0800

    revert pkg doc

--- pkg/query/logical/measure/measure_analyzer.go
+++ pkg/query/logical/measure/measure_analyzer.go
@@ -15,7 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Package measure implements execution operations for querying measure data.
 package measure
 
 import (
