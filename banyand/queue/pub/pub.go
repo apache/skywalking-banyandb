04dbe50dcb4118c6100d9a24fca80329b2d26743
Gao Hongtao <hanahmily@gmail.com>
2023-09-04 02:43:06 +0000

    Support remote dequeue
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>

--- banyand/queue/pub/pub.go
+++ banyand/queue/pub/pub.go
@@ -20,15 +20,17 @@ package pub
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"sync"
 	"time"
 
 	"go.uber.org/multierr"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	clusterv1 "github.com/apache/skywalking-banyandb/api/proto/banyandb/cluster/v1"
-	measurev1 "github.com/apache/skywalking-banyandb/api/proto/banyandb/measure/v1"
-	streamv1 "github.com/apache/skywalking-banyandb/api/proto/banyandb/stream/v1"
 	"github.com/apache/skywalking-banyandb/banyand/metadata"
 	"github.com/apache/skywalking-banyandb/banyand/metadata/schema"
 	"github.com/apache/skywalking-banyandb/banyand/queue"
@@ -66,8 +68,38 @@ func (p *pub) Serve() run.StopNotify {
 	return p.closer.CloseNotify()
 }
 
-func (*pub) Publish(_ bus.Topic, _ ...bus.Message) (bus.Future, error) {
-	panic("unimplemented")
+func (p *pub) Publish(topic bus.Topic, messages ...bus.Message) (bus.Future, error) {
+	var err error
+	f := &future{}
+	handleMessage := func(m bus.Message, err error) error {
+		r, errSend := messageToRequest(topic, m)
+		if errSend != nil {
+			return multierr.Append(err, fmt.Errorf("failed to marshal message %T: %w", m, errSend))
+		}
+		node := m.Node()
+		p.mu.Lock()
+		client, ok := p.clients[node]
+		p.mu.Unlock()
+		if !ok {
+			return multierr.Append(err, fmt.Errorf("failed to get client for node %s", node))
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		stream, errCreateStream := client.client.Send(ctx)
+		if err != nil {
+			return multierr.Append(err, fmt.Errorf("failed to get stream for node %s: %w", node, errCreateStream))
+		}
+		errSend = stream.Send(r)
+		if errSend != nil {
+			return multierr.Append(err, fmt.Errorf("failed to send message to node %s: %w", node, errSend))
+		}
+		f.clients = append(f.clients, stream)
+		return err
+	}
+	for _, m := range messages {
+		err = handleMessage(m, err)
+	}
+	return f, err
 }
 
 // NewBatchPublisher returns a new batch publisher.
@@ -96,7 +128,7 @@ func (p *pub) PreRun(context.Context) error {
 }
 
 type writeStream struct {
-	client clusterv1.Service_WriteClient
+	client clusterv1.Service_SendClient
 	cancel func()
 }
 
@@ -113,27 +145,14 @@ func (bp *batchPublisher) Close() (err error) {
 	return err
 }
 
-func (bp *batchPublisher) Publish(topic bus.Topic, message ...bus.Message) (bus.Future, error) {
-	var err error
-	for _, m := range message {
-		if !m.IsRemote() {
-			continue
-		}
-		r := &clusterv1.WriteRequest{
-			Topic:     topic.String(),
-			MessageId: uint64(m.ID()),
-		}
-		switch d := m.Data().(type) {
-		case *streamv1.InternalWriteRequest:
-			r.Request = &clusterv1.WriteRequest_Stream{Stream: d}
-		case *measurev1.InternalWriteRequest:
-			r.Request = &clusterv1.WriteRequest_Measure{Measure: d}
-		default:
-			err = multierr.Append(err, fmt.Errorf("invalid message type %T", d))
+func (bp *batchPublisher) Publish(topic bus.Topic, messages ...bus.Message) (bus.Future, error) {
+	for _, m := range messages {
+		r, err := messageToRequest(topic, m)
+		if err != nil {
+			err = multierr.Append(err, fmt.Errorf("failed to marshal message %T: %w", m, err))
 			continue
 		}
 		node := m.Node()
-
 		sendData := func() (success bool) {
 			if stream, ok := bp.streams[node]; !ok {
 				defer func() {
@@ -149,7 +168,7 @@ func (bp *batchPublisher) Publish(topic bus.Topic, message ...bus.Message) (bus.
 				}
 				errSend := stream.client.Send(r)
 				if errSend != nil {
-					err = multierr.Append(errSend, fmt.Errorf("failed to send message to node %s: %w", node, err))
+					err = multierr.Append(err, fmt.Errorf("failed to send message to node %s: %w", node, errSend))
 					return false
 				}
 				_, errRecv := stream.client.Recv()
@@ -170,7 +189,7 @@ func (bp *batchPublisher) Publish(topic bus.Topic, message ...bus.Message) (bus.
 		}
 		//nolint: govet
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		stream, errCreateStream := client.client.Write(ctx)
+		stream, errCreateStream := client.client.Send(ctx)
 		if err != nil {
 			err = multierr.Append(err, fmt.Errorf("failed to get stream for node %s: %w", node, errCreateStream))
 			continue
@@ -182,5 +201,63 @@ func (bp *batchPublisher) Publish(topic bus.Topic, message ...bus.Message) (bus.
 		_ = sendData()
 	}
 	//nolint: govet
-	return nil, err
+	return nil, nil
+}
+
+func messageToRequest(topic bus.Topic, m bus.Message) (*clusterv1.SendRequest, error) {
+	if !m.IsRemote() {
+		return nil, fmt.Errorf("message %d is not remote", m.ID())
+	}
+	r := &clusterv1.SendRequest{
+		Topic:     topic.String(),
+		MessageId: uint64(m.ID()),
+	}
+	message, ok := m.Data().(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("invalid message type %T", m)
+	}
+	anyMessage, err := anypb.New(message)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal message %T: %w", m, err)
+	}
+	r.Body = anyMessage
+	return r, nil
+}
+
+type future struct {
+	clients []clusterv1.Service_SendClient
+}
+
+func (l *future) Get() (bus.Message, error) {
+	if len(l.clients) < 1 {
+		return bus.Message{}, io.EOF
+	}
+	c := l.clients[0]
+	defer func() {
+		l.clients = l.clients[1:]
+	}()
+	resp, err := c.Recv()
+	if err != nil {
+		return bus.Message{}, err
+	}
+	return bus.NewMessage(
+		bus.MessageID(resp.MessageId),
+		resp.Body,
+	), nil
+}
+
+func (l *future) GetAll() ([]bus.Message, error) {
+	var globalErr error
+	ret := make([]bus.Message, 0, len(l.clients))
+	for {
+		m, err := l.Get()
+		if errors.Is(err, io.EOF) {
+			return ret, globalErr
+		}
+		if err != nil {
+			globalErr = multierr.Append(globalErr, err)
+			continue
+		}
+		ret = append(ret, m)
+	}
 }
