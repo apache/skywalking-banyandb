aad8a47e6af5604df615ea140ceb0b8602fef489
Suchith Krishna S Donni <sksdonni18@gmail.com>
2024-03-09 19:22:50 +0530

    added logic to clone route object

--- ui/src/components/Read/index.vue
+++ ui/src/components/Read/index.vue
@@ -191,7 +191,8 @@ watch(() => data.code, () => {
         data.codeStorage[index].params.code = data.code
     } else {
         route.params.code = data.code
-        data.codeStorage.push(JSON.parse(JSON.stringify(route)))
+        const clonedObject = clonedObject(route)
+        data.codeStorage.push(clonedObject)
     }
 })
 function initCode() {
@@ -216,6 +217,28 @@ orderBy:
   sort: SORT_UNSPECIFIED
 `)
     }
+}
+function cloneObject(obj){
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        const arrCopy = [];
+        for (let i = 0; i < obj.length; i++) {
+        arrCopy[i] = cloneObject(obj[i]);
+        }
+        return arrCopy;
+    }
+
+    const objCopy = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty !== undefined && obj.hasOwnProperty(key)) {
+            objCopy[key] = cloneObject(obj[key]);
+        }
+    }
+
+    return objCopy;
 }
 function changeCode(name, value) {
     let code = yamlToJson(data.code).data
