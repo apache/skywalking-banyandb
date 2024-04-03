3a438a5940d344684d56f0c56ef905efc35621a7
Gao Hongtao <hanahmily@gmail.com>
2024-04-03 15:37:49 +0800

    Add more test cases and fix some issues found in the tests (#426)

--- banyand/measure/block.go
+++ banyand/measure/block.go
@@ -610,10 +610,13 @@ func (bc *blockCursor) loadData(tmpBlock *block) bool {
 	}
 	bc.bm.field.columnMetadata = cfm
 	bc.bm.tagProjection = bc.tagProjection
-	tf := make(map[string]*dataBlock, len(bc.tagProjection))
+	var tf map[string]*dataBlock
 	for i := range bc.tagProjection {
 		for tfName, block := range bc.bm.tagFamilies {
 			if bc.tagProjection[i].Family == tfName {
+				if tf == nil {
+					tf = make(map[string]*dataBlock, len(bc.tagProjection))
+				}
 				tf[tfName] = block
 			}
 		}
