303ced0cd6583cbc3a7e54d19cb18214396b18d8
Sylvie-Wxr <sylvie_wu125@outlook.com>
2024-06-15 09:07:14 -0700

    add hash in set

--- pkg/meter/native/instruments.go
+++ pkg/meter/native/instruments.go
@@ -57,6 +57,7 @@ func (g *Gauge) Set(value float64, labelValues ...string) {
 	g.metrics[key] = metricWithLabelValues{
 		metricValue: value,
 		labelValues: tagValues,
+		seriesHash:  hash,
 	}
 }
 
