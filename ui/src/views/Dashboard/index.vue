1ad5f47f4b6b067b7b43de05478fc3efadab8ba5
Xinrui Wu <xinruiwu@Xinrui-Wus-Mac.local>
2024-08-19 22:33:31 +0800

    text quote change

--- ui/src/views/Dashboard/index.vue
+++ ui/src/views/Dashboard/index.vue
@@ -389,7 +389,8 @@ watchEffect(() => {
         </div>
         <div class="error-alert">
             <!-- Conditionally display the alert if hasMonitoring is false -->
-            <el-alert v-if="!hasMonitoring" title="Self-monitoring not available, please turn it on by setting \"--observability-modes=native\"."
+            <el-alert v-if="!hasMonitoring"
+                title="Self-monitoring not available, please turn it on by setting &quot;--observability-modes=native&quot;."
                 type="error" center show-icon :closable="false" />
         </div>
         <el-card shadow="always">
