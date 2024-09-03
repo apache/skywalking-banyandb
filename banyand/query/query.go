7f0bf8979aefcb5917f0c3e6e405125498d7ac89
Gao Hongtao <hanahmily@gmail.com>
2024-09-03 20:44:11 +0800

    Add slow query logging (#526)

--- banyand/query/query.go
+++ banyand/query/query.go
@@ -20,13 +20,31 @@ package query
 
 import (
 	"context"
+	"errors"
+	"time"
 
+	"go.uber.org/multierr"
+
+	"github.com/apache/skywalking-banyandb/api/common"
+	"github.com/apache/skywalking-banyandb/api/data"
 	"github.com/apache/skywalking-banyandb/banyand/measure"
 	"github.com/apache/skywalking-banyandb/banyand/metadata"
 	"github.com/apache/skywalking-banyandb/banyand/queue"
 	"github.com/apache/skywalking-banyandb/banyand/stream"
+	"github.com/apache/skywalking-banyandb/pkg/logger"
 	"github.com/apache/skywalking-banyandb/pkg/run"
 )
+
+type queryService struct {
+	metaService metadata.Repo
+	pipeline    queue.Server
+	log         *logger.Logger
+	sqp         *streamQueryProcessor
+	mqp         *measureQueryProcessor
+	tqp         *topNQueryProcessor
+	nodeID      string
+	slowQuery   time.Duration
+}
 
 // NewService return a new query service.
 func NewService(_ context.Context, streamService stream.Service, measureService measure.Service,
@@ -52,4 +70,33 @@ func NewService(_ context.Context, streamService stream.Service, measureService 
 		queryService:   svc,
 	}
 	return svc, nil
+}
+
+func (q *queryService) Name() string {
+	return moduleName
+}
+
+func (q *queryService) PreRun(ctx context.Context) error {
+	val := ctx.Value(common.ContextNodeKey)
+	if val == nil {
+		return errors.New("node id is empty")
+	}
+	node := val.(common.Node)
+	q.nodeID = node.NodeID
+	q.log = logger.GetLogger(moduleName)
+	return multierr.Combine(
+		q.pipeline.Subscribe(data.TopicStreamQuery, q.sqp),
+		q.pipeline.Subscribe(data.TopicMeasureQuery, q.mqp),
+		q.pipeline.Subscribe(data.TopicTopNQuery, q.tqp),
+	)
+}
+
+func (q *queryService) FlagSet() *run.FlagSet {
+	fs := run.NewFlagSet("query")
+	fs.DurationVar(&q.slowQuery, "slow-query", 0, "slow query threshold, 0 means no slow query log")
+	return fs
+}
+
+func (q *queryService) Validate() error {
+	return nil
 }
