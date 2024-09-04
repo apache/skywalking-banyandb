310e86725cdf39100e1fbe240ca79282f1f10d44
Leo Xie <stleox@qq.com>
2024-09-04 14:02:03 +0800

    [ospp] Supports measure aggregate function avg and min, and test cases. (#521)
    * [API] MeasureAggregateFunctionService Support
    
    * Supports measure aggregate function avg and min.
    
    ---------
    
    Co-authored-by: 吴晟 Wu Sheng <wu.sheng@foxmail.com>
    Co-authored-by: Gao Hongtao <hanahmily@gmail.com>

---
+++ banyand/measure/aggregate/min.go
@@ -0,0 +1,56 @@
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package aggregate
+
+// Min calculates the minimum value of elements.
+type Min[A, B Input, R Output] struct {
+	minimum R
+}
+
+// Combine takes elements to do the aggregation.
+// Min uses type parameter A.
+func (m *Min[A, B, R]) Combine(arguments Arguments[A, B]) error {
+	for _, arg0 := range arguments.arg0 {
+		switch arg0 := any(arg0).(type) {
+		case int64:
+			if R(arg0) < m.minimum {
+				m.minimum = R(arg0)
+			}
+		case float64:
+			if R(arg0) < m.minimum {
+				m.minimum = R(arg0)
+			}
+		default:
+			return errFieldValueType
+		}
+	}
+	return nil
+}
+
+// Result gives the result for the aggregation.
+func (m *Min[A, B, R]) Result() R {
+	return m.minimum
+}
+
+// NewMinArguments constructs arguments.
+func NewMinArguments[A Input](a []A) Arguments[A, Void] {
+	return Arguments[A, Void]{
+		arg0: a,
+		arg1: nil,
+	}
+}
