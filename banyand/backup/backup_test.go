5dc05e56197fec3875603aaa17a73b0d92d31cd0
Tsukilc <3168078770@qq.com>
2025-03-20 13:18:53 +0000

    fix: update backup_test.gotest logic because we will support s3

--- banyand/backup/backup_test.go
+++ banyand/backup/backup_test.go
@@ -39,7 +39,7 @@ func TestNewFS(t *testing.T) {
 		wantErr bool
 	}{
 		{"valid file scheme", "file:///tmp", false},
-		{"invalid scheme", "s3://bucket", true},
+		{"invalid scheme", "s3://bucket", false},
 		{"malformed URL", ":invalid", true},
 	}
 	for _, tt := range tests {
