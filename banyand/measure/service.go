7fd0e76968199d952851a85555482b6339aaa536
Eternity1824 <y4100606@163.com>
2025-05-20 15:23:39 +0800

    before solve the  merge conflict

--- banyand/measure/service.go
+++ banyand/measure/service.go
@@ -133,7 +133,7 @@ func (s *service) Role() databasev1.Role {
 func (s *service) PreRun(ctx context.Context) error {
 	s.l = logger.GetLogger(s.Name())
 	fs.SetThresholdProvider(s.pm)
-	s.lfs = fs.NewLocalFileSystemWithLogger(s.l)
+	s.lfs = fs.NewLocalFileSystemWithLoggerAndLimit(s.l, s.pm.GetLimit())
 	path := path.Join(s.root, s.Name())
 	s.snapshotDir = filepath.Join(path, storage.SnapshotsDir)
 	observability.UpdatePath(path)
