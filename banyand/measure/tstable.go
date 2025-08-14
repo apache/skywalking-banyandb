bfc96ce2072a7111ffb6eba0bda61383b8879ba4
Gao Hongtao <hanahmily@gmail.com>
2025-08-15 06:12:12 +0800

    Enhance flusher and introducer loops to support merging operations (#721)
    * Refactor flusher and sync loops to improve snapshot management and synchronization
    
    - Updated the `flusherLoop` to streamline snapshot handling and merging logic, enhancing performance during data flushing.
    - Introduced a new `syncInterval` option to control the periodic synchronization of data, improving efficiency in the `syncLoop`.
    - Added command-line flag support for `syncInterval` in the liaison service for better configuration.
    - Refactored related functions to ensure seamless integration of the new synchronization features.
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>

--- banyand/measure/tstable.go
+++ banyand/measure/tstable.go
@@ -125,16 +125,17 @@ func newTSTable(fileSystem fs.FileSystem, rootPath string, p common.Position,
 	return t, nil
 }
 
-// startLoopNoMerge starts the background loops without the merge loop.
-func (tst *tsTable) startLoopNoMerge(cur uint64) {
+// startLoopWithConditionalMerge starts the background loops with conditional merging based on flushTimeout.
+func (tst *tsTable) startLoopWithConditionalMerge(cur uint64) {
 	tst.loopCloser = run.NewCloser(1 + 3)
 	tst.introductions = make(chan *introduction)
 	flushCh := make(chan *flusherIntroduction)
+	mergeCh := make(chan *mergerIntroduction)
 	syncCh := make(chan *syncIntroduction)
 	introducerWatcher := make(watcher.Channel, 1)
 	flusherWatcher := make(watcher.Channel, 1)
-	go tst.introducerLoopWithSync(flushCh, syncCh, introducerWatcher, cur+1)
-	go tst.flusherLoopNoMerger(flushCh, introducerWatcher, flusherWatcher, cur)
+	go tst.introducerLoopWithSync(flushCh, mergeCh, syncCh, introducerWatcher, cur+1)
+	go tst.flusherLoop(flushCh, mergeCh, introducerWatcher, flusherWatcher, cur)
 	go tst.syncLoop(syncCh, flusherWatcher)
 }
 
