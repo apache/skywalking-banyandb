c9c6e9800facc6f6fedef2c13233411b86107917
peachisai <2581009893@qq.com>
2025-09-06 18:40:12 +0800

    fix

--- ui/src/components/common/data.js
+++ ui/src/components/common/data.js
@@ -17,6 +17,12 @@
 
 export const Last15Minutes = 900 * 1000;
 
+const Last30Minutes = 1800 * 1000;
+
+const LastHour = 3600 * 1000;
+
+const LastDay = 3600 * 1000 * 24;
+
 const LastWeek = 3600 * 1000 * 24 * 7;
 
 const LastMonth = 3600 * 1000 * 24 * 30;
@@ -24,20 +30,60 @@ const LastMonth = 3600 * 1000 * 24 * 30;
 const Last3Months = 3600 * 1000 * 24 * 90;
 
 export const Shortcuts = [
-  {
-    text: 'Last 15 minutes',
-    value: [new Date(new Date().getTime() - Last15Minutes), new Date()],
-  },
-  {
-    text: 'Last week',
-    value: [new Date(new Date().getTime() - LastWeek), new Date()],
-  },
-  {
-    text: 'Last month',
-    value: [new Date(new Date().getTime() - LastMonth), new Date()],
-  },
-  {
-    text: 'Last 3 months',
-    value: [new Date(new Date().getTime() - Last3Months), new Date()],
-  },
+    {
+        text: 'Last 15 minutes',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - Last15Minutes);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last 30 minutes',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - Last30Minutes);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last hour',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - LastHour);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last day',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - LastDay);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last week',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - LastWeek);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last month',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - LastMonth);
+            return [start, end];
+        },
+    },
+    {
+        text: 'Last 3 months',
+        value: () => {
+            const end = new Date();
+            const start = new Date(end.getTime() - Last3Months);
+            return [start, end];
+        },
+    },
 ];
