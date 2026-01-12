9b9065fad1be18b40ae6348a3a7046bf363f7d6d
peachisai <2581009893@qq.com>
2026-01-12 21:44:06 +0800

    fix

--- test/cases/topn/topn.go
+++ test/cases/topn/topn.go
@@ -48,5 +48,6 @@ var _ = g.DescribeTable("TopN Tests", verify,
 	g.Entry("using not equal in aggregation", helpers.Args{Input: "ne", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
 	g.Entry("using in operation in aggregation", helpers.Args{Input: "in", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
 	g.Entry("using not-in operation in aggregation", helpers.Args{Input: "not_in", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
-	g.Entry("max top3 order by desc with version merged", helpers.Args{Input: "aggr_version_merged", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
+	g.Entry("max top3 with version merged order by desc", helpers.Args{Input: "aggr_version_merged", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
+	g.Entry("non-aggr top3 with version merged order by asc", helpers.Args{Input: "aggr_version_merged", Duration: 25 * time.Minute, Offset: -20 * time.Minute}),
 )
