d654ba6a31ca728074f4ed5de89c8a4edf5d4e4e
eye-gu <734164350@qq.com>
2026-03-17 15:34:09 +0800

    fix tag value

--- ui/src/components/Read/index.vue
+++ ui/src/components/Read/index.vue
@@ -238,7 +238,7 @@ orderBy:
         if (tag.value[tagType[type]] === null) {
           dataItem[tag.key] = 'Null';
         } else {
-          dataItem[tag.key] = tag.value[tagType[type]]?.value || tag.value[tagType[type]];
+          dataItem[tag.key] = tag.value[tagType[type]]?.value ?? tag.value[tagType[type]];
         }
       }
       if (data.type === CatalogToGroupType.CATALOG_MEASURE && tableFields.length > 0) {
