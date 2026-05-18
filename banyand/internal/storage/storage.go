0dd5d684c7d5b82cab535551fa8ea09dbaafb440
mrproliu <741550557@qq.com>
2026-05-18 16:34:46 +0800

    align part sync timestamp to segment grid so part and sidx land in the same segment (#1136)

--- banyand/internal/storage/storage.go
+++ banyand/internal/storage/storage.go
@@ -112,6 +112,8 @@ type TSDB[T TSTable, O any] interface {
 	io.Closer
 	CreateSegmentIfNotExist(ts time.Time) (Segment[T, O], error)
 	SelectSegments(timeRange timestamp.TimeRange) ([]Segment[T, O], error)
+	// SegmentInterval returns the current segment interval rule.
+	SegmentInterval() IntervalRule
 	Tick(ts int64)
 	UpdateOptions(opts *commonv1.ResourceOpts)
 	TakeFileSnapshot(dst string) (bool, error)
